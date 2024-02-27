32336ea86ea64a57f06058e559c6192f71aed1cf
chrischo3600 <chrischo3600@gmail.com>
2024-02-27 16:53:23 -0500

    non troll 3.2 working

--- Lab3.2.X/main.c
+++ Lab3.2.X/main.c
@@ -35,48 +35,81 @@ int main(void)
     uart2_init(9600);
     
     int fail = 0;
-    lcd_locate(0,0);
-    lcd_printf_d("Recv fail: %d times", fail);
+    
     int a = 0;
     while (U2STAbits.URXDA) {
         a = U2RXREG & 0x00FF;
     }
-    lcd_locate(0,2);
-    lcd_printf_d("bi");
     while(1) {
        //Acknowledge messages
         // Read the start bit
         // Wait for start bit
-        uint8_t* start_byte = malloc(sizeof(uint8_t));
-        while (uart2_recv(start_byte) != 0);         // Receive start bit
+        
+        lcd_locate(0,0);
+        lcd_printf_d("Recv fail: %d times", fail);
+        uint8_t buffer[259] = {0};
+        buffer[0] = -1;
+        buffer[1] = -1;
+        buffer[2] = -1;
+        uint8_t i = 0;
+        
+        for (i = 0; i < 4; i++) {
+            while (uart2_recv(&buffer[i]) != 0);
+        }
+        lcd_clear();
+        for (i = 4; i < 4 + buffer[3]; i++) {
+            while (uart2_recv(&buffer[i]) != 0);
+        }
+        
+        //while (uart2_recv(buffer[i]) == 0) {
+        //    i++;
+        //};
+        
+
+        uint16_t crc_rec = 0;
+        crc_rec = (buffer[1] << 8) | buffer[2];
+        
+        uint8_t N = 0;
+        N = buffer[3];
+
+        char message[N]; 
+        uint16_t crc = 0;
+        int j;
+        // reading message
+        for (j = 4; j < (N + 4); j++) {
+            message[j - 4] = (char) buffer[j];
+            crc = crc_update(crc, buffer[j]);
+        }
+        message[j - 4] = '\0';
+ 
+        lcd_locate(0,6);
+        lcd_printf_d("start_byte: %x, %x, %x", buffer[0], buffer[1], buffer[2]);
+        lcd_locate(0,1);
+        lcd_printf_d("crc_rec: %x", crc_rec);
+        lcd_locate(0,2);
+        lcd_printf_d("crc: %x", crc);
+        lcd_locate(0,3);
+        lcd_printf_d("length: %d", N);
+        lcd_locate(0,4);
+        lcd_printf_d(message);
         lcd_locate(0,5);
-        lcd_printf_d("start_byte: %x", *start_byte);
+        lcd_printf_d("i: %d", i);
         
-        
-        if (*start_byte != 0) {
+        if (crc_rec == crc) {
+            uart2_send_8(MSG_ACK);
+            fail = 0;
+            lcd_locate(0,0);
+            lcd_printf("Recv fail: %d times", fail);
+        } else {
             fail++;
+            lcd_locate(0,0);
+            lcd_printf("Recv fail: %d times", fail);
+            int a = 0;
             while (U2STAbits.URXDA) {
                 a = U2RXREG & 0x00FF;
             }
             uart2_send_8(MSG_NACK);
-            continue;
         }
-        
-        lcd_locate(0,2);
-        lcd_printf_d("hi");
-//        while(1);
-//        uint8_t* crc_byte1 = malloc(sizeof(uint8_t));
-////        *crc_byte1 = 0;
-//        
-//        while (uart2_recv(crc_byte1) != 0);
-//        lcd_locate(0,3);
-//        lcd_printf_d("%x", *crc_byte1);
-//        
-//        uint8_t* crc_byte2 = malloc(sizeof(uint8_t));
-//        while (uart2_recv(crc_byte2) != 0);
-//        lcd_locate(0,4);
-//        lcd_printf_d("%x", *crc_byte2);
-                
     }
 }	
 
