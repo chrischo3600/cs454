e734e6fbc90101fb8870ac705ab6b1a01967b164
chrischo3600 <chrischo3600@gmail.com>
2024-04-21 15:53:06 -0400

    Lab 8 Day 2

--- Lab8.X/pid.py
+++ Lab8.X/pid.py
@@ -3,7 +3,7 @@ from scipy import signal
 
 
 # Define the filter parameters
-Wn = (20)/(20/2)  # in hz filter
+Wn = (5)/(20/2)  # in hz filter
 fs = 20  # Sample rate in hz
 N = 3
 b, a = signal.butter(N, Wn, btype='lowpass', fs=fs)
